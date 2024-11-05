6f5d2b834fc2602feff43d86b1123c75047aff9a
Charlie Chiu <charlie910417@gmail.com>
2024-11-05 10:38:40 +0800

    finish argmax

--- src/argmax.s
+++ src/argmax.s
@@ -25,12 +25,27 @@ argmax:
     li t6, 1
     blt a1, t6, handle_error
 
-    lw t0, 0(a0)
+    lw t1, 0(a0)  # t1 store the initial current max value
+    li t2, 0            # t2 store max value index
+    li t3, 0            # t3 loop counter
 
-    li t1, 0
-    li t2, 1
+
 loop_start:
-    # TODO: Add your own implementation
+    lw t4, 0(a0)
+    blt t1, t4, update_max #If current max smaller than current element then branch
+    j next_element
+
+update_max:
+    mv t1, t4
+    mv t2, t3
+
+next_element:
+    addi t3, t3, 1
+    addi a0, a0, 4
+    blt t3,a1,loop_start
+
+    mv a0,t2
+    ret
 
 handle_error:
     li a0, 36
